a6be9514b4539611cff95c464c76db131b823581
AhmedAlganzory <ahmedalganzorygfx@gmail.com>
2026-09-08 14:49:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ try:
             result = df[df[id_column].str.contains(search_query, na=False)]
 
             if not result.empty:
-                st.success("🎉 تم العثور على بيانات الشهادة بنجاح:")
+                st.success("🎉 تم العثور على بيانات المعلم:")
 
                 # عرض النتائج في شكل بطاقات أنيقة
                 for idx, row in result.iterrows():
